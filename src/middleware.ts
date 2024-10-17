d2b1a411d40eb695e74cd747022d459b067de626
Rashed Zaman <jmrashed@gmail.com>
2024-10-17 10:57:30 +0600

    add log in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,14 +8,25 @@ export async function middleware(req: NextRequest): Promise<NextResponse> {
   // Define the guest routes that should not be accessible for logged-in users
   const guestRoutes: string[] = ['/login', '/register'];
 
+  console.log('-----token-----')
+  console.log(token);
+
   if (!token) {
+    console.log('-----not token-----')
+    console.log(token);
+
     // User is not logged in
     if (!guestRoutes.includes(req.nextUrl.pathname)) {
+      console.log('-----guest route-----')
+      console.log(token);
+    
       return NextResponse.redirect(new URL('/login', req.url));
     }
   } else {
     // User is logged in
     if (guestRoutes.includes(req.nextUrl.pathname)) {
+      console.log('-----not guest route route-----')
+      console.log(token);
       // If trying to access a guest route while logged in, redirect to home
       return NextResponse.redirect(new URL('/', req.url));
     }
